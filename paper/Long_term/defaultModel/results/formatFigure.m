353ffa594e5006e0bd5a25e2384d5c77f4d6ec7f
ChennoShen239 <2392992780@qq.com>
2025-10-12 11:40:49 +0800

    chore: sync local changes

--- paper/Long_term/defaultModel/results/formatFigure.m
+++
@@ -1,5 +0,0 @@
-set(gcf,'PaperUnits','inches');
-set(gcf,'PaperSize', [8 8]);
-set(gcf,'PaperPosition',[0.5 0.5 7 7]);
-set(gcf,'PaperPositionMode','Manual');
-fontsize(14, "points");
--- paper/Long_term/defaultModel/results/loadBinary.m
+++
@@ -1,6 +0,0 @@
-function [ out ] = loadBinary(fname, type, sz)
-    fileid = fopen(fname, 'r');
-    out = fread(fileid, [prod(sz), 1], type);
-    fclose(fileid);
-    out = reshape(out, sz);
-end
